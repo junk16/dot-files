ae5c0ef8da89243eb7730d9b6be15b4566e78807
jun yamada <jun.16@mac.com>
2026-06-30 18:18:12 +0900

    Update vim related

--- config/nvim/lua/plugins/mcphub.lua
+++ config/nvim/lua/plugins/mcphub.lua
@@ -1,17 +1,29 @@
 return {
-
 	"ravitemer/mcphub.nvim",
+	enabled = false,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
 	build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
 	config = function()
-		require("mcphub").setup({
-			extensions = {
-				avante = {
-					make_slash_commands = true,
-				},
-			},
-		})
+		require("mcphub").setup()
 	end,
 }
+
+-- return {
+--
+-- 	"ravitemer/mcphub.nvim",
+-- 	dependencies = {
+-- 		"nvim-lua/plenary.nvim",
+-- 	},
+-- 	build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+-- 	config = function()
+-- 		require("mcphub").setup({
+-- 			extensions = {
+-- 				avante = {
+-- 					make_slash_commands = true,
+-- 				},
+-- 			},
+-- 		})
+-- 	end,
+-- }
